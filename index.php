2975ca6064d59b5621386c58d12e6ee39d30be75
Carlos Lorenzon <carloslorenzon@gmail.com>
2017-10-04 09:16:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 include './config.php';
 include './ldap.php';
 header('Content-Type: application/json');
-if (!isset($_REQUEST['acao'])) {
+if (!isset($_REQUEST['acao']) or (!isset($_REQUEST['hash']) or $_REQUEST['hash'] != _LDAP_HASH)) {
+    header('HTTP/1.0 403 Forbidden');
     exit;
 }
-
 $ldap = new acl_ldap();
 if ($_REQUEST['acao'] == 'lista') {
     $user = $ldap->getUsers();
